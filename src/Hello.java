2b57248f23a99bc4cd57e6ca49e116faf1cfbf01
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-17 21:44:14 -0500

    add and or operators

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,25 @@
 public class Hello {
+
     public static void main(String[] args) {
-        System.out.println("Hello, Tim ");
-        System.out.print("hello, world");
+        System.out.println("Hello, Sunny ");
+
+        boolean isAlien = true;
+        if (isAlien == false) {
+            System.out.println("its not an alien");
+        }else{
+            System.out.println("its an alien");
+        }
+
+        int topScore  = 60;
+        if (topScore <  100){
+            System.out.println("you got the high score");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore || topScore < 100){
+            System.out.println("you passed!");
+        }else{
+            System.out.println("you failed");
+        }
     }
 }
